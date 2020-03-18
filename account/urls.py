f62e922479d5a2a44c28b5c820186b5c991426e4
Qfy <718310611@qq.com>
2020-03-18 23:40:16 +0800

    添加了登录，注册，退出模块

---
+++ account/urls.py
@@ -0,0 +1,12 @@
+
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+
+    path('signup/', views.signup,name='注册页面'),
+    path('login/', views.login,name='登录页面'),
+    path('logout/', views.logout,name='退出页面'),
+]
